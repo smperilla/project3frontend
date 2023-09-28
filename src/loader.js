8f5269785f7c9309f10ef0eac559af90e59ff77a
ryland <ryland@MacBook-Pro-4.lan1>
2023-09-28 15:56:44 -0700

    more changes

--- src/loader.js
+++ src/loader.js
@@ -1,7 +1,7 @@
 const url = 'http://localhost:4000'
 
 export const usersLoader = async ()=>{
-    const res = await fetch(url+'/users')
+    const res = await fetch(url+'/users/6515dc9ffc1ca272ca121d28')
     const users = await res.json()
     return users
 }
