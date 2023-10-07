6a0517cec3bbdf040e3f062c2fb0a83614b5b10f
ryland <ryland@MacBook-Pro-4.localdomain>
2023-10-07 03:50:13 -0700

    links

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Test = () => {
   return (
-    <div>Test</div>
+    <div>
+      <Link to="/login">Log In</Link>
+      <Link to="/signup">Sign Up</Link>
+    </div>
   )
 }
 
